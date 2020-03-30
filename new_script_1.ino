b6c85cbd89fa956d7e8206b77b848f99d2029cab
azz-design <53371325+azz-design@users.noreply.github.com>
2020-03-30 16:09:58 +0530

    upload script

---
+++ new_script_1.ino
@@ -0,0 +1,40 @@
+
+#include <DHT.h>  // Including library for dht
+
+#include <ESP8266WiFi.h>
+ 
+String apiKey = "Your API of thingsspeak";   
+
+const char *ssid =  "#####";     
+const char *pass =  "##";
+const char* server = "##";
+
+#define DHTPIN 0         
+ 
+DHT dht(DHTPIN, DHT11);
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       dht.begin();
+ 
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+ 
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ 
+}
+ 
+
